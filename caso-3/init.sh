4f98c628169154297469cd8f4f634c0f39cd8f5a
Lucas Garcia <lu.garcia@alumno.um.edu.ar>
2024-07-11 18:03:50 -0300

    WIP init.sh

--- caso-3/init.sh
+++ caso-3/init.sh
@@ -1,44 +1,81 @@
 #!/bin/bash
 
-echo "Starting init.sh"
-
-encoded_metabase_mail=$(echo -n "${METABASE_MAIL}" | base64)
-encoded_metabase_password=$(echo -n "${METABASE_PASSWORD}" | base64)
-encoded_metabase_db_user=$(echo -n "${METABASE_DB_USER}" | base64)
-encoded_metabase_db_password=$(echo -n "${METABASE_DB_PASSWORD}" | base64)
-encoded_mobility_db_user=$(echo -n "${MOBILITY_DB_USER}" | base64)
-encoded_mobility_db_password=$(echo -n "${MOBILITY_DB_PASSWORD}" | base64)
-
-sed -i '' "s/METABASE_MAIL: .*/METABASE_MAIL: $encoded_metabase_mail/" secrets.yaml
-sed -i '' "s/METABASE_PASSWORD: .*/METABASE_PASSWORD: $encoded_metabase_password/" secrets.yaml
-sed -i '' "s/METABASE_DB_USER: .*/METABASE_DB_USER: $encoded_metabase_db_user/" secrets.yaml
-sed -i '' "s/METABASE_DB_PASSWORD: .*/METABASE_DB_PASSWORD: $encoded_metabase_db_password/" secrets.yaml
-sed -i '' "s/MOBILITY_DB_USER: .*/MOBILITY_DB_USER: $encoded_mobility_db_user/" secrets.yaml
-sed -i '' "s/MOBILITY_DB_PASSWORD: .*/MOBILITY_DB_PASSWORD: $encoded_mobility_db_password/" secrets.yaml
-
-kubectl apply -f "${all_yaml}"
+LOG_FILE="/var/log/init_script.log"
 
 
-# Wait for MySQL pod to be ready
-while [[ $(kubectl get pods -l app=mysql -n metabase -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do 
-  echo "Waiting for MySQL pod to be ready..."
-  sleep 10
-done
+echo "Starting init.sh" | tee -a $LOG_FILE
 
-# Download SQL file
-wget --no-check-certificate -O /tmp/google-mobility.sql "${sql_file_url}"
+sudo apt-get update
+sudo apt-get install expect
 
-# Get MySQL pod name
-MYSQL_POD=$(kubectl get pods -l app=mysql -n metabase -o jsonpath="{.items[0].metadata.name}")
+# Paso 1: Inicializar kubectl
+echo "Paso 1: Inicializando kubectl" | tee -a $LOG_FILE
 
-# Copy SQL file to MySQL pod
-kubectl cp /tmp/google-mobility.sql metabase/"$MYSQL_POD":/tmp/google-mobility.sql
+rancher login --context "c-6bdnb:p-svfjj" --token ${rancher_token} https://rancher.kube.um.edu.ar/v3
+rancher context current
+rancher project ls | grep -q LucasGarcia-project || rancher project create LucasGarcia-project
+rancher context switch LucasGarcia-project
+rancher kubectl config view --raw=true | install -D -m 640 /dev/stdin ~/.kube/config
 
-# Execute SQL script
-kubectl exec -it "$MYSQL_POD" -n metabase -- mysql -u root -p"${metabase_db_password}" <<EOF
-CREATE DATABASE IF NOT EXISTS mobility;
-USE mobility;
-source /tmp/google-mobility.sql;
-EOF
+# Paso 2: Crear NAMESPACE
+echo "Paso 2: Creando NAMESPACE" | tee -a $LOG_FILE
+rancher namespace create metabase
+kubectl ns metabase
 
-echo "Setup completed successfully!"
+echo "Paso 3: encoding 64" | tee -a $LOG_FILE
+encoded_metabase_mail=$(echo -n "${METABASE_MAIL}" | base64) | tee -a $LOG_FILE
+encoded_metabase_password=$(echo -n "${METABASE_PASSWORD}" | base64) | tee -a $LOG_FILE
+encoded_metabase_db_user=$(echo -n "${METABASE_DB_USER}" | base64) | tee -a $LOG_FILE
+encoded_metabase_db_password=$(echo -n "${METABASE_DB_PASSWORD}" | base64) | tee -a $LOG_FILE
+encoded_mobility_db_user=$(echo -n "${MOBILITY_DB_USER}" | base64) | tee -a $LOG_FILE
+encoded_mobility_db_password=$(echo -n "${MOBILITY_DB_PASSWORD}" | base64) | tee -a $LOG_FILE
+
+
+mkdir -p kube_yamls | tee -a $LOG_FILE
+
+# Echoing variables to files and logging
+{
+  echo "${configmap_yaml}" >"./kube_yamls/configmap.yaml"
+  echo "${deploy_yaml}" >"./kube_yamls/deploy.yaml"
+  echo "${ingress_yaml}" >"./kube_yamls/ingress.yaml"
+  echo "${network_yaml}" >"./kube_yamls/network.yaml"
+  echo "${pvc_yaml}" >"./kube_yamls/pvc.yaml"
+  echo "${secret_yaml}" >"./kube_yamls/secret.yaml"
+  echo "${service_yaml}" >"./kube_yamls/service.yaml"
+} | tee -a $LOG_FILE
+
+sed -i '' "s/METABASE_MAIL: .*/METABASE_MAIL: $encoded_metabase_mail/" ./kube_yamls/secrets.yaml
+sed -i '' "s/METABASE_PASSWORD: .*/METABASE_PASSWORD: $encoded_metabase_password/" ./kube_yamls/secrets.yaml
+sed -i '' "s/METABASE_DB_USER: .*/METABASE_DB_USER: $encoded_metabase_db_user/" ./kube_yamls/secrets.yaml
+sed -i '' "s/METABASE_DB_PASSWORD: .*/METABASE_DB_PASSWORD: $encoded_metabase_db_password/" ./kube_yamls/secrets.yaml
+sed -i '' "s/MOBILITY_DB_USER: .*/MOBILITY_DB_USER: $encoded_mobility_db_user/" ./kube_yamls/secrets.yaml
+sed -i '' "s/MOBILITY_DB_PASSWORD: .*/MOBILITY_DB_PASSWORD: $encoded_mobility_db_password/" ./kube_yamls/secrets.yaml
+# kubectl apply -f tmp/ | tee -a $LOG_FILE
+
+# # Wait for MySQL pod to be ready and log
+# {
+#   while [[ $(kubectl get pods -l app=mysql -n metabase -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do
+#     echo "Waiting for MySQL pod to be ready..."
+#     sleep 10
+#   done
+# } | tee -a $LOG_FILE
+
+# # Download SQL file and log
+# wget --no-check-certificate -O /tmp/google-mobility.sql "${sql_file_url}" | tee -a $LOG_FILE
+
+# # Get MySQL pod name and log
+# MYSQL_POD=$(kubectl get pods -l app=mysql -n metabase -o jsonpath="{.items[0].metadata.name}") | tee -a $LOG_FILE
+
+# # Copy SQL file to MySQL pod and log
+# kubectl cp /tmp/google-mobility.sql metabase/"$MYSQL_POD":/tmp/google-mobility.sql | tee -a $LOG_FILE
+
+# # Execute SQL script and log
+# {
+#   kubectl exec -it "$MYSQL_POD" -n metabase -- mysql -u root -p"${METABASE_DB_PASSWORD}" <<EOF
+# CREATE DATABASE IF NOT EXISTS mobility;
+# USE mobility;
+# source /tmp/google-mobility.sql;
+# EOF
+# } | tee -a $LOG_FILE
+
+# echo "Setup completed successfully!" | tee -a $LOG_FILE
--- caso-3/vm_kubernetes.tf
+++ caso-3/vm_kubernetes.tf
@@ -44,17 +44,31 @@ variable "mobility_db_password" {
   sensitive   = true
 }
 
+variable "rancher_token" {
+  description = "Bearer Token for Rancher"
+  type        = string
+  sensitive   = true
+}
+
 
 resource "openstack_compute_instance_v2" "docker_vm" {
   name              = "kube_vm"
   image_id          = data.openstack_images_image_v2.docker.id
   flavor_id         = data.openstack_compute_flavor_v2.small.id
-  key_pair          = mac
+  key_pair          = "mac"
   security_groups   = ["default"]
   availability_zone = "nodos-amd-2022"
 
-  user_data = templatefile("init.sh.tpl", {
-    all_yaml = file("${path.module}/all.yaml",
+  user_data = templatefile("init.sh", {
+    configmap_yaml = file("${path.module}/configmap.yaml"),
+    deploy_yaml = file("${path.module}/deploy.yaml"),
+    ingress_yaml = file("${path.module}/ingress.yaml"),
+    namespace_yaml = file("${path.module}/namespace.yaml"),
+    network_yaml = file("${path.module}/network.yaml"),
+    pvc_yaml = file("${path.module}/pvc.yaml"),
+    secret_yaml = file("${path.module}/secrets.yaml"),
+    service_yaml = file("${path.module}/services.yaml"),
+    rancher_token = var.rancher_token,
     METABASE_MAIL = var.metabase_mail,
     METABASE_PASSWORD = var.metabase_password,
     METABASE_DB_USER = var.metabase_db_user,
@@ -62,8 +76,8 @@ resource "openstack_compute_instance_v2" "docker_vm" {
     MOBILITY_DB_USER = var.mobility_db_user,
     MOBILITY_DB_PASSWORD = var.mobility_db_password,
     sql_file_url = "https://drive.google.com/uc?export=download&id=1AC2uvs6f2t4qrhXpz5XowSxoVXR3TfvG"
+  }
     )
-  })
 
   network {
     name = "net_umstack"
